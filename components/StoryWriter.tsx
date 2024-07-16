84552c596c41dd5133b82f2b033ff912ab806269
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2024-07-16 22:51:11 +0300

    Creating API endpoint

--- components/StoryWriter.tsx
+++ components/StoryWriter.tsx
@@ -11,9 +11,39 @@ import {
 import { Textarea } from './ui/textarea';
 import { Button } from './ui/button';
 
+const storiesPath = 'public/stories';
+
 function StoryWriter() {
   const [story, setStory] = useState<string>('');
   const [pages, setPages] = useState<number>();
+  const [progress, setProgress] = useState('');
+  const [runStarted, setRunStarted] = useState<boolean>(false);
+  const [runFinished, setRunFinished] = useState<boolean | null>(null);
+  const [currentTool, setCurrentTool] = useState('');
+
+  const runScript = async () => {
+    setRunStarted(true);
+    setRunFinished(null);
+    setProgress('');
+
+    const response = await fetch('/api/run-script', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ story, pages, path: storiesPath }),
+    });
+
+    if (response.ok && response.body) {
+      // Handle Streams from API
+      console.log('Streamiong started');
+    } else {
+      setRunFinished(true);
+      setRunStarted(false);
+      console.error('Failed to start streaming');
+    }
+  };
+
   return (
     <div className='flex flex-col container'>
       <section className='flex flex-1 flex-col border border-purple-300 rounded-md p-10 space-y-2'>
@@ -33,7 +63,7 @@ function StoryWriter() {
           <SelectContent className='w-full'>
             {Array.from({ length: 10 }, (_, i) => (
               <SelectItem key={i} value={String(i + 1)}>
-                {i + 1}
+                {i + 1} page
               </SelectItem>
             ))}
           </SelectContent>
@@ -41,12 +71,44 @@ function StoryWriter() {
         <Button
           className='w-full bg-purple-400'
           size='lg'
-          disabled={!story || !pages}
+          disabled={!story || !pages || runStarted}
+          onClick={runScript}
         >
           Genarate Story
         </Button>
       </section>
-      <section className='flex-1 pb-5 mt-5'></section>
+      <section className='flex-1 pb-5 mt-5'>
+        <div className='flex flex-col-reverse w-full space-y-2 bg-gray-800 rounded-md text-gray-200 font-mono p-10 h-96 overflow-y-auto'>
+          <div>
+            {runFinished === null && (
+              <>
+                <p className='animate-pulse mr-5'>
+                  Is waiting for you to generate a story above
+                </p>
+                <br />
+              </>
+            )}
+            <span className='mr-5'>{'>>'}</span>
+            {progress}
+          </div>
+          {/* current tool */}
+          {currentTool && (
+            <div className='py-18'>
+              <span className='mr-5'>{'--- (Current Tool) ---'}</span>
+              {currentTool}
+            </div>
+          )}
+
+          {runStarted && (
+            <div>
+              <p className='animate-in mr-5'>
+                {'--- (AI Storyteller Has Started!!) ---'}
+              </p>
+              <br />
+            </div>
+          )}
+        </div>
+      </section>
     </div>
   );
 }
